14e2086e66ed6f6781f75d07b3ed6ec4cb27c030
CannSahin <cansahin019@gmail.com>
2025-02-01 13:49:37 +0300

    Added functionality to display tasks with Edit and Delete buttons, and toggle task form visibility.

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ const descriptionInput = document.getElementById("description-input");
 const taskData = [];
 let currentTask = {};
 
+
+
 openTaskFormBtn.addEventListener("click", () =>
   taskForm.classList.toggle("hidden")
 );
@@ -39,5 +41,23 @@ taskForm.addEventListener("submit", (e) => {
     date: dateInput.value,
     description: descriptionInput.value,
   };
- 
+
+   if (dataArrIndex === -1) {
+    taskData.unshift(taskObj);
+  }
+
+  taskData.forEach(({id, title, date, description}) => {
+      tasksContainer.innerHTML += `
+        <div class="task" id="${id}">
+          <p><strong>Title:</strong> ${title}</p>
+          <p><strong>Date:</strong> ${date}</p>
+          <p><strong>Description:</strong> ${description}</p>
+          <button type="button" class="btn">Edit</button>
+          <button type="button" class="btn">Delete</button>
+        </div>
+      `
+    } 
+  );
+
+  taskForm.classList.toggle("hidden");
 });
